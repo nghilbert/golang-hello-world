d5c76cf407a5faafc86651645c019c31cbbdb455
Nate <nathanghilbert@gmail.com>
2025-11-15 13:12:17 -0600

    added descriptive comments

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
+// Our Go module is defined here.
+// Go modules are similar to projects in Java.
 module example/hello
 
 go 1.24.4
--- hello.go
+++ hello.go
@@ -1,6 +1,9 @@
+// Our main Go package is defined here.
+// Go packages are similar to Java packages — they group related code
 package main
 
-// Go's standard IO library. fmt stands for "format"
+// Import Go's standard I/O library.
+// "fmt" stands for "format" and provides formatted I/O functions
 import "fmt"
 
 func main() {
