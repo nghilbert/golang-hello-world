e45da2f6f5af469d7d12f9c38e3d337ac11345d5
Nate <nathanghilbert@gmail.com>
2025-11-15 13:03:39 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# golang-hello-world
+
+A simple "Hello, World!" program written in Go.
+
+## How to Run
+
+```bash
+go run main.go
+```
---
+++ hello.go
@@ -0,0 +1,8 @@
+package main
+
+// Go's standard IO library. fmt stands for "format"
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
